b268457d45f0e2cf33a4e3a3d74d69ec8ffd69dc
Michał Łaszczewski <michal@laszczewski.com>
2021-04-28 17:17:51 +0200

    wasNeverOnline index

--- model.js
+++ model.js
@@ -47,7 +47,8 @@ const PublicSessionInfo = definition.model({
       type: Date
     },
     user: {
-      type: User
+      type: User,
+      defaultValue: false
     }
   },
   indexes: {
@@ -58,7 +59,28 @@ const PublicSessionInfo = definition.model({
       property: "online",
       function: async function(input, output) {
         const mapper =
-            (obj) => obj.online &&
+            (obj) => obj.online && ({ id: obj.id, to: obj.id })
+        await input.table('accessControl_PublicSessionInfo').onChange(
+            (obj, oldObj) => output.change(obj && mapper(obj), oldObj && mapper(oldObj))
+        )
+      }
+    },
+    neverOnline: {
+      property: ["online", "lastOnline"],
+      function: async function(input, output) {
+        const mapper =
+            (obj) => (!obj.online) && (!obj.lastOnline) &&
+                ({ id: obj.id, to: obj.id })
+        await input.table('accessControl_PublicSessionInfo').onChange(
+            (obj, oldObj) => output.change(obj && mapper(obj), oldObj && mapper(oldObj))
+        )
+      }
+    },
+    wasOnline: {
+      property: ["online", "lastOnline"],
+      function: async function(input, output) {
+        const mapper =
+            (obj) => (obj.online || obj.lastOnline) &&
                 ({ id: obj.id, to: obj.id })
         await input.table('accessControl_PublicSessionInfo').onChange(
             (obj, oldObj) => output.change(obj && mapper(obj), oldObj && mapper(oldObj))
